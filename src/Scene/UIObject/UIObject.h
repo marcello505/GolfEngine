f1d9e58e8094512765af58398ff798ad7618e761
lucasvanrooij19@gmail.com <lucasvanrooij19@gmail.com>
2022-11-02 14:14:07 +0100

    added Skeleton

---
+++ src/Scene/UIObject/UIObject.h
@@ -0,0 +1,16 @@
+//
+// Created by Lucas on 2-11-2022.
+//
+
+#ifndef GOLFENGINE_UIOBJECT_H
+#define GOLFENGINE_UIOBJECT_H
+#include "../GameObject.h"
+
+class UIObject : public GameObject {
+private:
+    float _width;
+    float _height;
+};
+
+
+#endif //GOLFENGINE_UIOBJECT_H
