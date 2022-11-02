81f8797287cf8a928df0653b7360d2bb4a982b4b
Conner Linskens <conner@linskens.nl>
2022-11-02 13:55:54 +0100

    Added Camera skeleton

---
+++ src/Scene/Camera.h
@@ -0,0 +1,20 @@
+//
+// Created by conner on 11/2/2022.
+//
+
+#ifndef SPC_PROJECT_CAMERA_H
+#define SPC_PROJECT_CAMERA_H
+
+#include "GameObject.h"
+// TODO replace with include
+class Color;
+
+class Camera : public GameObject {
+private:
+    Color _backgroundColor;
+    float aspectWidth;
+    float aspectHeight;
+};
+
+
+#endif //SPC_PROJECT_CAMERA_H
