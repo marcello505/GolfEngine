c17d381089b378554cda6c02dff0398f9a732acb
Olaf van Pinxteren <82908515+OlafAvans@users.noreply.github.com>
2022-11-02 13:18:19 +0100

    fixed

--- src/Input/ActionMap.cpp
+++ src/Input/ActionMap.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "ActionMap.h"
+
+void ActionMap::addAction(std::string name){};
+void ActionMap::addInputKeyToAction(std::string action, InputKey inputKey){};
+bool ActionMap::isJustPressed(std::string action){return false;};
+bool ActionMap::isJustReleased(std::string action){return false;};
+bool ActionMap::isPressed(std::string action){return false;};
+bool ActionMap::isReleased(std::string action){return false;};
