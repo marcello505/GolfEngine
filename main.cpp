3c86c9378da85766138de6f0881a75da645e677c
Marcello Haddeman <marcellohaddeman@gmail.com>
2022-11-02 11:23:27 +0100

    Add doctest and test project

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
-#include <iostream>
-#include <SDL.h>
 
 int main()
 {
-    auto d = SDL_GetTicks();
-
-
-    std::cout << d << std::endl;
     return 0;
 }
---
+++ tests/SampleTests.cpp
@@ -0,0 +1,6 @@
+#include <doctest.h>
+
+TEST_CASE("SAMPLE TESTCASE"){
+    bool var = true;
+    CHECK(var);
+}
