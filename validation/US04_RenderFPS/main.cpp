a82025af64231a810699d8977083b70720e087e2
lucasvanrooij19@gmail.com <lucasvanrooij19@gmail.com>
2022-11-19 21:00:52 +0100

    ffixed main

--- validation/US04_RenderFPS/main.cpp
+++ validation/US04_RenderFPS/main.cpp
@@ -14,20 +14,14 @@ int main(int argc, char* argv[]){
     GameLoop gameLoop {};
 
     gameLoop.useDefaultServices();
-    gameLoop.setInputService(nullptr);
-//    gameLoop.setPhysicsService(nullptr);
-//    gameLoop.setAudioService(nullptr);
+    gameLoop.setPhysicsService(nullptr);
+    gameLoop.setAudioService(nullptr);
 
 
 
 
    auto rs = GolfEngine::Services::Render::getService();
 
-
-    auto* rect = new RectDrawable(Rect2(Vector2(100,100), Vector2(100,100)),
-                            Transform(Vector2(0,0), 0, Vector2(1,1)),
-                            Vector2(50,75),
-                            Color(255,0,0,255));
 
     std::string fps =   std::to_string(gameLoop.time->getRenderFps());
 
