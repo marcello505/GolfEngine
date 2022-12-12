0ac976f12acec35d943fe38bb6d33ff680151ff8
Marcello Haddeman <marcellohaddeman@gmail.com>
2022-12-12 20:09:09 +0100

    Add extra TestCase for Floats

--- tests/Utilities/Random.cpp
+++ tests/Utilities/Random.cpp
@@ -3,7 +3,7 @@
 #include "Utilities/Random.h"
 
 TEST_SUITE("Random"){
-    TEST_CASE("setSeed() always results in the same number"){
+    TEST_CASE("setSeed() always results in the same integer"){
         //Arrange
         auto seed = GolfEngine::Utilities::Random::setRandomSeed();
 
@@ -16,6 +16,24 @@ TEST_SUITE("Random"){
 
         //Assert
         CHECK_NE(firstResult, seccondResult);
+        CHECK_NE(seccondResult, thirdResult);
+        CHECK_EQ(firstResult, thirdResult);
+    }
+
+    TEST_CASE("setSeed() always results in the same float"){
+        //Arrange
+        auto seed = GolfEngine::Utilities::Random::setRandomSeed();
+
+        //Act
+        auto firstResult = GolfEngine::Utilities::Random::getRealRange(0.0f, 100.f);
+        auto seccondResult = GolfEngine::Utilities::Random::getRealRange(0.f, 100.f);
+
+        GolfEngine::Utilities::Random::setSeed(seed);
+        auto thirdResult = GolfEngine::Utilities::Random::getRealRange(0.f, 100.f);
+
+        //Assert
+        CHECK_NE(firstResult, seccondResult);
+        CHECK_NE(seccondResult, thirdResult);
         CHECK_EQ(firstResult, thirdResult);
     }
 }
