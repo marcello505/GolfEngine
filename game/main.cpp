0260c8b5868aae64965df6b326363aa3de36800d
lucasvrtje <lucasvanrooij19@gmail.com>
2022-12-13 09:55:04 +0100

    fix SDL.h inculde error

--- game/main.cpp
+++ game/main.cpp
@@ -3,11 +3,14 @@
 #include "Core/GameLoop.h"
 #include "Services/Singletons/RenderSingleton.h"
 
+//TODO find something to fix this
+#include <SDL.h>
+
 // Game includes
 #include "scenes/PlayerTestScene.h"
 
 
-int main(){
+int main(int argc, char* argv[]){
     GameLoop gameLoop {};
     gameLoop.useDefaultServices();
 
@@ -41,4 +44,5 @@ int main(){
     GolfEngine::Services::Render::getService()->setScreenSize(1280, 720);
 
     gameLoop.start();
+    return 0;
 }
