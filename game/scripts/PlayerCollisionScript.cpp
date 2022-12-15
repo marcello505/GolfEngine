6779c6ac09f8296ffc2af7544f7d474dd2642da3
Conner Linskens <conner@linskens.nl>
2022-12-15 11:46:23 +0100

    SceneManager include fix

--- game/scripts/PlayerCollisionScript.cpp
+++ game/scripts/PlayerCollisionScript.cpp
@@ -2,13 +2,13 @@
 // Created by conner on 12/13/2022.
 //
 
+#include "Core/SceneManager.h"
 #include "PlayerCollisionScript.h"
 #include <iostream>
-#include "Core/SceneManager.h"
 
 void PlayerCollisionScript::onStart() {
-//    auto& gm = GolfEngine::SceneManager::GetSceneManager().getCurrentScene().getGameObjectWithTag("GameManager");
-//    _gameManager = &gm.getComponent<GameManagerScript>();
+    auto& gm = GolfEngine::SceneManager::GetSceneManager().getCurrentScene().getGameObjectWithTag("GameManager");
+    _gameManager = &gm.getComponent<GameManagerScript>();
 
     if(_gameObject->get().hasComponent<PlayerMovementScript>())
         _playerMovement = &_gameObject->get().getComponent<PlayerMovementScript>();
