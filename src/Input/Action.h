611750a3f5a83ebf6b198be5a0f8e630a61c38ef
Conner Linskens <conner@linskens.nl>
2022-12-18 13:19:28 +0100

    Added comments to action map

--- src/Input/Action.h
+++ src/Input/Action.h
@@ -5,16 +5,11 @@
 #ifndef GOLFENGINE_ACTION_H
 #define GOLFENGINE_ACTION_H
 #include <string>
-class Action {
-public:
+struct Action {
     std::string name {};
-    std::string keyRefString;
+    std::string keyRefString {};
     bool pressed {};
     bool justInput {};
-/*    bool operator==(const Action& other) const
-    {
-        return (name == other.name);
-    }*/
 };
 #endif //GOLFENGINE_ACTION_H
 
