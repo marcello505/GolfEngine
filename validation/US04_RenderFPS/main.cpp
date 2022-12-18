c7d9dd9f900440faa0511fee4cbd7fbc6ba55137
Conner Linskens <conner@linskens.nl>
2022-12-18 13:03:37 +0100

    Fixed minor merge errors

--- validation/US04_RenderFPS/main.cpp
+++ validation/US04_RenderFPS/main.cpp
@@ -3,8 +3,6 @@
 #include <Services/SDLRenderService.h>
 #include "Services/SDLInputService.h"
 #include "Services/Singletons/RenderSingleton.h"
-    auto text = TextDrawable(Vector2(100,50),
-                                 Transform(Vector2(0,0), 0,Vector2(1,1)  ),  fps, 36 ,Color(), R"(..\..\..\validation\US04_RenderFPS\files\roman.ttf)", gameLoop
 #include "TextDrawable.h"
 
 
